21ac827e7f2f4895c54ef33e4b9e600b72019074
KotlarovMiroslav <kotlarovmiroslav@gmail.com>
2026-03-15 16:42:31 +0200

    added the functions clrscr and gotoxy

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-int main (){
-    printf("\033[1mHello World!\n");
-    printf("Hello again!\n");
-    return 0;
+#include <stdio.h>
+void clrscr(){
+    printf("\033[2J\033[H");
 }
 
+void gotoxy(int x, int y){
+    printf("\033[%d;%dH",y,x);
+}
+
+
+int main (){
+    printf("\033[44;31m");
+    printf("Hi!");
+    printf("\033[0m\n");
+    clrscr();
+    gotoxy(20,10);
+    printf("TestCase\n");
+    return 0;
+}
